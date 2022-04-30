643046d6e96505b68296da2f03c7f9be3b8e7217
938938 <938kjh@gmail.com>
2022-05-01 04:04:56 +0900

    queue

--- section21.js
+++ section21.js
@@ -4,7 +4,7 @@
 // 후입선출 원칙을 따르는 데이터 모음. 좀 더 압축적인 데이터 구조.
 // 함수 호출을 다루는 상황, 실행 취소/재실행, 인터넷 방문 기록, 알고리즘 등에 사용.
 // 자바스크립트에 내장된 기능은 아님(but 코드는 쉽게 짤 수 있음)
-// 배열로 대신할 수 있음
+// 배열로 대신할 수 있음(그러나 스택이 더 가벼움)
 // 스택에서는 push와 pop이 상수값을 가져야 함. 단일 연결리스트의 pop은 상수값의 시간을 가지지 않음
 
 // ex)
@@ -14,12 +14,12 @@ function factorial(x){
 }
 
 // 배열로 스택 만들기
-let stack1 = [];
-stack1.push("google");
-stack1.push("instagram");
-stack1.push("youtube");
+let stack = [];
+stack.push("google");
+stack.push("instagram");
+stack.push("youtube");
 // stack = ["google", "instagram", "youtube"];
-stack1.pop();
+stack.pop();
 // stack = ["google", "instagram"];
 // push,pop 방식. unshift, shift도 비슷한 방식이지만 비효율적임.
 // 효율성만 따질 때 : 후입선출만 지키면 되는 경우 배열보다 연결리스트가 더 효율적임
@@ -62,3 +62,50 @@ class Stack {
 }
 
 // 시간 복잡도 : insertion - O(1), Removal - O(1), Searching - O(n), Access = O(n);
+
+// 큐 Queue
+// 선입선출 원칙의 데이터 구조.
+// 입장 대기, 백그라운드 작업, 업로드/다운로드 작업, 프린트 대기열 등에 사용
+// 배열로 대신할 수 있음(그러나 큐가 더 가벼움)
+// enqueue : 추가 , dequeue : 제거
+
+let q = [];
+q.push(1);
+q.push(2);
+q.push(3);
+// q = [1,2,3];
+q.shift();
+// q = [2,3];
+
+// class Node{}
+
+class Queue {
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val){ // 추가
+    let newNode = new Node(val);
+    if(!this.first){
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+  dequeue(){
+    if(!this.first) return null;
+    let temp = this.first;
+    if(this.first === this.last){
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+// 시간 복잡도 : Insertion - O(1), Removal - O(1), Searching - O(n), Access - O(n)
