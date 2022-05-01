2931611f8cfe8348ca7243a5ec11efd134b9c89d
938938 <938kjh@gmail.com>
2022-05-02 05:19:21 +0900

    Trees

--- Trees.js
+++ Trees.js
@@ -25,9 +25,52 @@ class BinarySearchTree {
   constructor(){
     this.root = null;
   }
+  insert(value){ // 값을 입력, 트리의 적당한 위치에 추가
+    let newNode = new Node(value);
+    if(!this.root){ // 트리가 비어있을 경우 루트로 추가함
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while(true){
+        if(value===current.value) return undefined;
+        // 기존에 있던 값과 같은 값을 추가하는 경우 : 무시하거나(undefined/null) 혹은 count 변수를 증가시켜 횟수를 새는 방법 등이 있음
+        if(value<current.value){
+          if(!current.left){ // 왼쪽 노드에 값이 존재하지 않을 경우
+            current.left = newNode;
+            return this;
+          }
+          current = current.left; // 존재할 경우 한칸 더 내려와서 다시 실행
+        } else { // value > current.value일 때
+          if(!current.right){
+            current.right = newNode;
+            return this;
+          }
+          current = current.right;
+        }
+      }
+    }
+  }
+  contains(value){ // 해당 값이 트리에 있는지를 탐색
+    if(!this.root) return false;
+    let current = this.root,
+        found = false; // 해당 요소를 찾았는지 추적
+    while(current && !found){ // 안에 값이 없는 경우, 반복을 끝내기 위해 current 조건 추가
+      if(value<current.value){
+        current = current.left;
+      } else if (value>current.value){
+        current = current.right;
+      } else { // 값을 찾았을 경우(값이 같은 경우)
+        found = true;
+      }
+    }
+    return false; // 값을 찾지 못한 경우
+  }
 }
 
 let tree = new BinarySearchTree();
 tree.root = new Node(10);
 tree.root.right = new Node(15);
 tree.root.left = new Node(5);
+
+// 시간 복잡도 : Insertion - O(log n), Searching - O(log n)
