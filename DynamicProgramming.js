eb8bfe44afd162219f077d2a12b3c335a07a3e11
938938 <938kjh@gmail.com>
2022-05-14 01:21:21 +0900

    dynamicProgramming

--- DynamicProgramming.js
+++ DynamicProgramming.js
@@ -21,3 +21,29 @@ function fib(n){
 }
 
 // 시간 복잡도 : O(2^n)
+// 해당 방식으로는 작업을 반복하게 되어서 비효율적임
+
+// Memo에 값을 저장하는 방법
+// 배열이나 객체인 데이터를 저장할 구조를 만든 다음, 시간이 오래 걸리는 함수를 실행, 해당 값을 저장.
+
+function fib_dynamic(n, memo=[]){ // memo : 하위 문제들에 대해 이미 찾은 답을 저장하는 장소. 한번이라도 풀었다면 그 답을 저장.
+  if(memo[n] !== undefined) return memo[n];
+  if(n <= 2) return 1;
+  let res = fib(n-1, memo) + fib(n-2, memo);
+  memo[n] = res;
+  return res;
+}
+
+// 시간 복잡도 : O(n)
+
+// 타뷸레이션 Tabulation : 상향적 접근
+// 공간에 제약을 덜 받는 편
+
+function fib_tabulation(n){
+  if(n<=2) return 1;
+  let fibNums = [0,1,1];
+  for(let i = 3; i<=n; i++){
+    fibNums[i] = fibNums[i-1] + fibNums[i-2];
+  }
+  return fibNums[n];
+}
