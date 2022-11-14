7d8c64cc57f7cbf635ea2b0c6aee91675af71bf7
938938 <938kjh@gmail.com>
2022-11-14 23:59:47 +0900

    완전탐색

--- Test/Algorithm.js
+++ Test/Algorithm.js
@@ -14,3 +14,37 @@ coins.forEach((coin)=>{
 })
 
 console.log(result);
+
+// 완전 탐색
+// 모든 경우의 수 계산
+
+// ex. 숫자 야구 게임
+// 친구가 생각하는 수와 자릿수까지 일치하면 스트라이크, 숫자만 일치하면 볼
+const input = [
+  [123, 1, 1],
+  [356, 1, 0],
+  [327, 2, 0],
+  [489, 0, 1]
+]
+
+const answer = [];
+
+// 불필요한 수 제거
+for (let i = 123; i<=987, i++){
+  const temp = Strig(1)
+  if(temp.includes('0') || temp[0] === temp[1] || temp[1] === temp[2] || temp[0] === temp[2]) continue;
+  answer.push(temp)
+}
+
+input.forEach(data=>{
+  const temp = Stirng(data[0]);
+  for(let i = answer.length - 1 ; i >= 0; i--){
+    let [strike, ball] = [0, 0]
+    for(let j = 0; j<3; j++){
+      if(answer[i][j] === temp[j]) strike++;
+      if(answer[i][(j+1)%3] === temp[j] || answer[i][(j+2)%3] === temp[j]) ball++
+    }
+    if(strike !== data[1] || ball !== data[2]) answer.splice(i, 1)
+  }
+})
+
