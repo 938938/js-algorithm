30b77f031d97fb70901f727087d601470773d049
938938 <938kjh@gmail.com>
2022-11-15 23:52:26 +0900

    완전탐색

--- Test/Algorithm.js
+++ Test/Algorithm.js
@@ -1,3 +1,5 @@
+// 출처 : https://nohack.tistory.com/137
+
 // 1. 탐욕 알고리즘
 // 현재 상황에서 문제 해결을 위해 가장 좋아보이는 것을 고르는 방법
 // 문제 해결에 대한 창의적인 접근 필요
@@ -15,7 +17,7 @@ coins.forEach((coin)=>{
 
 console.log(result);
 
-// 완전 탐색
+// 2. 완전 탐색
 // 모든 경우의 수 계산
 
 // ex. 숫자 야구 게임
@@ -30,13 +32,16 @@ const input = [
 const answer = [];
 
 // 불필요한 수 제거
-for (let i = 123; i<=987, i++){
+for (let i = 123; i<=987; i++){
   const temp = Strig(1)
+  // 숫자 중복, 0 제거
   if(temp.includes('0') || temp[0] === temp[1] || temp[1] === temp[2] || temp[0] === temp[2]) continue;
   answer.push(temp)
 }
 
+// 경우의 수 확인
 input.forEach(data=>{
+  // 예측 숫자
   const temp = Stirng(data[0]);
   for(let i = answer.length - 1 ; i >= 0; i--){
     let [strike, ball] = [0, 0]
@@ -44,7 +49,68 @@ input.forEach(data=>{
       if(answer[i][j] === temp[j]) strike++;
       if(answer[i][(j+1)%3] === temp[j] || answer[i][(j+2)%3] === temp[j]) ball++
     }
+    // 스트라이크와 볼의 개수가 맞지 않는 경우 모두 제거
     if(strike !== data[1] || ball !== data[2]) answer.splice(i, 1)
   }
 })
 
+
+// 3. 시뮬레이션
+// 문제 유형의 한 종류. 출제자가 요구하는 사항에 따라 한 단계씩 단계별로 문제를 해결
+
+// ex. 게임 개발
+
+// 캐릭터의 위치, 바라보는 방향
+let [position, direction] = [[1,1],0]
+// 움직임 값
+const mx = [-1, 0, 1, 0]
+const my = [0, 1, 0, -1]
+// 맵
+const map = [
+  [1,1,1,1],
+  [1,0,0,1],
+  [1,1,0,1],
+  [1,1,1,1]
+]
+let visit = [
+  [0,0,0,0],
+  [0,0,0,0],
+  [0,0,0,0],
+  [0,0,0,0],
+]
+
+// 시작 시점 방문 처리
+visit[position[0]][position[1]] = 1;
+let count = 1
+// 반시계 방향으로 회전
+function turn(){
+  direction = direction -1
+  if(direction<0) direction = 3
+}
+// 회전 횟수
+let turnCount = 0;
+while (true) {
+  // 반시계 방향 회전
+  turn();
+  turnCount++;
+  // 회전 후 이동 계산 (Next X, Y)
+  [nx, ny] = [position[0] + mx[direction], position[1] + my[direction]];
+  // 이동할 방향이 벽이 아니면서, 방문하지 않은 경우
+  if (map[nx][ny] === 0 && visit[nx][ny] === 0) {
+    position = [nx, ny];
+    visit[nx][ny] = 1;
+    count++;
+    turnCount = 0;
+  }
+  // 네 방향 모두 확인한 경우
+  if (turnCount === 4) {
+    // 돌아갈 위치 계산 (Next X, Y)
+    [nx, ny] = [position[0] - mx[direction], position[1] - my[direction]];
+    // 돌아갈 곳이 벽인 경우
+    if (map[nx][ny] === 1) break;
+    else {
+      position = [nx, ny];
+      turnCount = 0;
+    }
+  }
+}
