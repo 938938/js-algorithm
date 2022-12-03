32653965815b2f643307a33d131d1e56fc012f5c
938938 <938kjh@gmail.com>
2022-12-03 14:53:54 +0900

    ::문제 풀이

--- Test/lv1.js
+++ Test/lv1.js
@@ -56,3 +56,46 @@ function solution(absolutes, signs) {
   return answer;
 }
 
+// 나머지가 1이 되는 수 찾기
+// https://school.programmers.co.kr/learn/courses/30/lessons/87389
+
+function solution(n) {
+  let answer = 0;
+  while(n%answer !== 1){
+    // n을 answer로 나눴을 때 구하는 값(1)이 나오지 않을 경우 answer을 1씩 증가.
+      answer++;
+  }
+  return answer;
+}
+
+// 행렬의 덧셈
+// https://school.programmers.co.kr/learn/courses/30/lessons/12950
+
+function solution(arr1, arr2) {
+  let answer = []; // 빈 배열 생성
+  for(let i = 0; i<arr1.length; i++){
+      answer[i] = []; // 배열 안에 배열 생성
+      for(let j = 0; j<arr1[i].length; j++){
+          answer[i].push(arr1[i][j] + arr2[i][j])
+          // 빈 배열에 arr1과 arr2의 행렬을 더해서 반환
+      }
+  }
+  return answer;
+}
+
+// 평균 구하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12944
+
+function solution(arr) {
+  let answer = (arr.reduce((a, b)=>a+b))/arr.length // 행렬의 값을 모두 더해서 길이로 나누기
+  return answer;
+}
+
+// 같은 숫자는 싫어
+// https://school.programmers.co.kr/learn/courses/30/lessons/12906
+
+function solution(arr){
+  return arr.filter((elm,i)=> elm !== arr[i+1]);
+  // 배열 안의 다음 아이템과 같은지 확인 후 아닌 것만(중복되지 않은 것만) filter 처리
+}
+
