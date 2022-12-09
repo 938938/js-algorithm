5051be48ca91e2e94f6b0a2fb98ad6c77422d742
938938 <938kjh@gmail.com>
2022-12-09 23:52:36 +0900

    ::문제 풀이

--- Test/lv2.js
+++ Test/lv2.js
@@ -8,3 +8,17 @@ function solution(s) {
   // 정렬된 배열의 가장 처음과 마지막 요소를 문자열로 변환
   return answer;
 }
+
+// 최솟값 만들기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12941
+
+function solution(A,B){
+  let answer = 0;
+  let a = A.sort((a,b)=>a-b) // A 배열을 오름차순으로 정렬
+  let b = B.sort((a,b)=>b-a) // B 배열을 오름차순으로 정렬
+  for(let i=0; i<a.length; i++){
+      answer += a[i]*b[i] // 각 배열에서 가장 낮은 요소끼리 곱하여 answer에 더하기
+  }
+  return answer;
+}
+
